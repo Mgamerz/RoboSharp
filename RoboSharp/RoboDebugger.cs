d2ca0db0cab8bee8c7c83991d28f8d8b0abf27c9
Mgamerz <mgamerz2@gmail.com>
2021-11-22 19:42:31 -0700

    Add RoboDebugger.cs

---
+++ RoboSharp/RoboDebugger.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace RoboSharp
+{
+    public sealed class RoboDebugger
+    {
+        private static readonly Lazy<RoboDebugger> instance = new Lazy<RoboDebugger>(() => new RoboDebugger());
+
+        private RoboDebugger()
+        {
+
+        }
+
+        public static RoboDebugger Instance
+        {
+            get { return instance.Value; }
+        }
+
+        public EventHandler<DebugMessageArgs> DebugMessageEvent;
+
+        public class DebugMessageArgs : EventArgs
+        {
+            public object Message { get; set; }
+        }
+
+        private void RaiseDebugMessageEvent(object message)
+        {
+            DebugMessageEvent?.Invoke(this, new DebugMessageArgs
+            {
+                Message = message
+            });
+        }
+
+        internal void DebugMessage(object data)
+        {
+            RaiseDebugMessageEvent(data);
+        }
+    }
+}
